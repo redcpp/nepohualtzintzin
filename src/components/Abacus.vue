7a2b5b263baf104244c0b36edd9c78fa5c8a8341
Diego Said Anaya Mancilla <anayamd96@gmail.com>
2017-01-14 12:08:44 -0600

    Start: Refactoring

---
+++ src/components/Abacus.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="abacus w-90">
+    <div v-for="col in cols" class="stick w-10">
+      <span v-for="row in topRows">
+        <item :value="5" :initActive="0" @toogle="sum"></item>
+      </span >
+    </div>
+    <hr>
+     <div v-for="col in cols" class="stick w-10">
+      <span v-for="row in bottomRows">
+        <item :value="1" :initActive="0" @toogle="sum"></item>
+      </span >
+    </div>
+  </div>
+</template>
+
+<script>
+import Item from './Item'
+
+export default {
+  name: 'abacus',
+  props: ['initCols'],
+  components: {
+    Item
+  },
+  data () {
+    return {
+      number: 0,
+      cols: this.initCols,
+      topRows: 3,
+      bottomRows: 4
+    }
+  },
+  methods: {
+    sum: function (active, value) {
+      let inc = active ? value : -value
+      this.number += inc
+      this.$emit('numberChange', this.number)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.abacus {
+  margin: auto;
+  border: 2px solid #aaa;
+}
+
+.stick {
+  display: inline-block;
+  background-color: #f9f7e8;
+  border: 1px solid rgba(0,0,0,.3);
+}
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="item" @click="toogleActive" :class="{ active: active }">
+    {{value}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'item',
+  props: ['value', 'initActive'],
+  data () {
+    return {
+      active: this.initActive
+    }
+  },
+  methods: {
+    toogleActive: function () {
+      this.active ^= 1
+      this.$emit('toogle', this.active, this.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.item {
+  cursor: pointer;
+  display: block;
+  padding: 15px;
+  margin: 5px auto;
+  background-color: transparent;
+  border: none;
+  outline: none;
+}
+
+.item.active {
+  background-color: #b7e3e4;
+}
+</style>
