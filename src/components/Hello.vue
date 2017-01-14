db16785f279e5e61559f0f16213bbeb124738cf4
Diego Said Anaya Mancilla <anayamd96@gmail.com>
2017-01-13 22:38:08 -0600

    Start project with abacus skeleton

---
+++ src/components/Hello.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="hello">
+    <h1>Current value: {{ number }}</h1>
+    <div class="abacus">
+      <div v-for="stick in data.top" class="stick w-10">
+        <span v-for="item in stick" class="coin">
+          {{item.value}}
+        </span >
+      </div>
+      <hr>
+      <div v-for="stick in data.bottom" class="stick w-10">
+        <span v-for="item in stick" class="coin">
+          {{item.value}}
+        </span >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const COLS = 10
+export default {
+  name: 'hello',
+  data () {
+    return {
+      number: 0,
+      data: {
+        top: Array(COLS).fill(Array(3).fill({value: 5, active: 0})),
+        bottom: Array(COLS).fill(Array(4).fill({value: 1, active: 0}))
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.app {
+  text-align: center;
+}
+
+.abacus {
+  margin: auto;
+  width: 600px;
+  border: 2px solid #aaa;
+}
+
+.stick {
+  display: inline-block;
+  background-color: #f9f7e8;
+  border: 1px solid rgba(0,0,0,.3);
+  padding: 10px;
+}
+
+.coin {
+  cursor: pointer;
+  display: block;
+  padding: 10px;
+}
+
+.coin.active {
+  background-color: #b7e3e4;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
